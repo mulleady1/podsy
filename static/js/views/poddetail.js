ab5c8a48527a61c349ea914d52041cd3d2bd9dbd
mulleady1 <kyle.mulleady@gmail.com>
2015-04-07 20:09:33 -0700

    Pod detail template

---
+++ static/js/views/poddetail.js
@@ -0,0 +1,38 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone'
+], function($, _, Backbone) {
+    var PodDetailView = Backbone.View.extend({
+        template: _.template($('#pod-detail-template').html()),
+        events: {
+            'click .upvote': 'toggleUpvote',
+            'click .downvote': 'toggleDownvote',
+            'click .podtitle > a': 'listen',
+            'click .podtitle > .glyphicon': 'toggleFavorite'
+        },
+        initialize: function() {
+            this.listenTo(this.model, 'change', this.render);
+            this.listenTo(this.model, 'listen', this.listen);
+        },
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        },
+        listen: function() {
+            app.listenView.render(this.model.toJSON());
+        },
+        toggleUpvote: function() {
+            this.model.toggleUpvote();
+        },
+        toggleDownvote: function() {
+            this.model.toggleDownvote();
+        },
+        toggleFavorite: function() {
+            if (!app.loggedIn) return;
+            this.model.toggleFavorite();
+        }
+    });
+
+    return PodDetailView;
+});
