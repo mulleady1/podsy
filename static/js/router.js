44c2f7b41ff136dd4fd92743903fdb4a238f17c3
Kyle Mulleady <kyle.mulleady@gmail.com>
2015-06-08 17:29:10 -0700

    routechange

--- static/js/router.js
+++ static/js/router.js
@@ -20,28 +20,24 @@ define([
             'account/tags/favs/':      'tagsByFav',
             'users/:username/':        'userByUsername'
         },
-        index: function() {
+        execute: function(callback, args, name) {
             this.pauseAudio();
+            this.collapseMobileNav();
             $('.view').hide();
+            callback.apply(this, args);
+        },
+        index: function() {
             $('#pods-view').show();
             app.loadInitialPods();
         },
         newPod: function() {
-            this.pauseAudio();
-            $('.view').hide();
-            this.collapseMobileNav();
             $('#upload-view').show();
         },
         podById: function(id) {
-            this.pauseAudio();
-            $('.view').hide();
             app.podDetailView.show(app.pods.get(id));
         },
         podsByFav: function() {
-            this.pauseAudio();
             if (!app.loggedIn) return;
-            $('.view').hide();
-            this.collapseMobileNav();
             $('#pods-view').show();
             app.headerView.show({ name: 'My favorite pods', description: '' });
             $.get('/account/pods/favs/').then(function(data) {
@@ -49,10 +45,6 @@ define([
             });
         },
         podsByCategory: function(name) {
-            this.pauseAudio();
-            $('.view').hide();
-            this.collapseMobileNav();
-
             function f() {
                 app.categories.each(function(cat) {
                     if (cat.get('name').toLowerCase() == name) {
@@ -67,9 +59,7 @@ define([
             } else {
                 app.categories.fetch({
                     reset: true,
-                    success: function() {
-                        f();
-                    }
+                    success: f
                 });
             }
             $('#pods-view').show();
@@ -78,55 +68,33 @@ define([
             });
         },
         categories: function() {
-            this.pauseAudio();
-            $('.view').hide();
             $('#categories-view').show();
             if (!app.categories.length) {
                 app.categories.fetch({ reset: true });
             }
         },
         newCategory: function() {
-            this.pauseAudio();
-            $('.view').hide();
-            this.collapseMobileNav();
             $('#category-view').show();
         },
         signin: function() {
-            this.pauseAudio();
-            $('.view').hide();
-            this.collapseMobileNav();
             $('#signin-view').show();
         },
         signup: function() {
-            this.pauseAudio();
-            $('.view').hide();
-            this.collapseMobileNav();
             $('#signup-view').show();
         },
         account: function() {
-            this.pauseAudio();
             if (!app.loggedIn) return;
-            $('.view').hide();
-            this.collapseMobileNav();
             $('#account-view').show();
         },
         about: function() {
-            this.pauseAudio();
-            $('.view').hide();
-            this.collapseMobileNav();
             $('#about-view').show();
         },
         tags: function() {
-            this.pauseAudio();
-            $('.view').hide();
-            this.collapseMobileNav();
             $('#tags-view').show();
             $('#tags-view h3.title').html('Tags');
             app.tags.fetch({ reset: true });
         },
         tagByName: function(tagName) {
-            this.pauseAudio();
-            $('.view').hide();
             $('#pods-view').show();
             $.get('/tags/{tagName}/'.replace('{tagName}', tagName)).then(function(data) {
                 app.headerView.show({ name: data.name, description: data.description });
@@ -134,32 +102,25 @@ define([
             });
         },
         tagsByFav: function() {
-            this.pauseAudio();
             if (!app.loggedIn) return;
-            $('.view').hide();
-            this.collapseMobileNav();
             $('#tags-view').show();
             $('#tags-view h3.title').html('My favorite tags');
             $.get('/account/tags/favs/').then(function(data) {
                 app.tags.reset(data);
             });
         },
-        collapseMobileNav: function() {
-            this.pauseAudio();
-            var button = $('button.navbar-toggle[aria-expanded="true"]');
-            if (button.length) {
-                button.click();
-            }
-        },
         userByUsername: function(username) {
-            this.pauseAudio();
-            $('.view').hide();
-            this.collapseMobileNav();
             $('#users-view').show();
             $.get('/users/{username}/'.replace('{username}', username)).then(function(data) {
                 app.headerView.show({ name: data.username, description: data.description });
                 $('#users-view').html('<pre>'+app.toJson(data)+'</pre>');
             });
+        },
+        collapseMobileNav: function() {
+            var button = $('button.navbar-toggle[aria-expanded="true"]');
+            if (button.length) {
+                button.click();
+            }
         },
         pauseAudio: function() {
             var audioEl = app.podDetailView.audioEl;
